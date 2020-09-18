7a54814f990c81e30a23513687571ab830ccb2fa
chan_star <traintion9@gmail.com>
2020-09-19 00:23:10 +0900

    before IR multi-curve structuring

--- interest_rate/multiple_curves.py
+++ interest_rate/multiple_curves.py
@@ -44,6 +44,10 @@ dates = []
 for i in range((end_date - start_date).days + 1):
     dates.append(start_date + timedelta(days=i))
 LIBOR_products = LIBOR_products[(LIBOR_products.index >= start_date) & (LIBOR_products.index <= end_date)]
+LIBOR_products[DEPOSITS] *= 0.01
+LIBOR_products[FRA] *= 0.01
+LIBOR_products[SWAP_RATE] *= 0.01
+LIBOR_products[BASIS_SWAP_RATE] *= 0.0001
 
 # define maturities/tenors
 deposit_maturities = [ql.Period(1, ql.Days), ql.Period(3, ql.Days), ql.Period(7, ql.Days),
@@ -78,75 +82,99 @@ for date in dates:
     today = ql.Date(date.day, date.month, date.year)
     LIBOR_product = LIBOR_products[LIBOR_products.index == date]
     LIBOR_product = LIBOR_product.dropna(axis='columns').T
-    for product in IR_products:
-        # convert quote object
-        if product == DEPOSITS:
-            _deposits = LIBOR_product.loc[product]
-            _deposits.index = deposit_maturities
-            _deposits[QUOTE] = _deposits.apply(lambda x: ql.SimpleQuote(x[date]), axis=1)
-            depositHelpers = [
-                ql.DepositRateHelper(ql.QuoteHandle(_deposits.loc[mat][QUOTE]), mat, settlementDays, calendar_country,
-                                     ql.ModifiedFollowing, False, ql.Actual360()) for mat in deposit_maturities]
-        if product == FRA:
-            _fra = LIBOR_product.loc[product].reset_index()
-            _fra[TENOR] = _fra.apply(lambda x: int(x['index'].split('X')[1].replace('F=', '')) - int(
-                x['index'].split('X')[0].replace('USD', '')), axis=1)
-            _fra3L = dict(zip(FRA_3L_maturities,
-                              _fra[_fra[TENOR] == 3].apply(lambda x: ql.SimpleQuote(x[date]), axis=1).to_list()))
-            _fra6L = dict(zip(FRA_6L_maturities,
-                              _fra[_fra[TENOR] == 6].apply(lambda x: ql.SimpleQuote(x[date]), axis=1).to_list()))
-            fra3L_Helpers = [
-                ql.FraRateHelper(ql.QuoteHandle(_fra3L[mat]), mat[0], mat[1], settlementDays, calendar_country,
-                                 ql.ModifiedFollowing, False, ql.Actual360()) for mat in FRA_3L_maturities]
-            fra6L_Helpers = [
-                ql.FraRateHelper(ql.QuoteHandle(_fra6L[mat]), mat[0], mat[1], settlementDays, calendar_country,
-                                 ql.ModifiedFollowing, False, ql.Actual360()) for mat in FRA_6L_maturities]
-        if product == EURODOLLAR_FUTURES:
-            _eurodollar = LIBOR_product.loc[product]
-            _eurodollar[MAT] = _eurodollar.apply(lambda x: EURODOLLAR_maturities[x.name], axis=1)
-            dayCounter = ql.Thirty360()
-            convexityAdjustment = ql.QuoteHandle(ql.SimpleQuote(0.01))
-            lengthInMonths = 1
-            eurodollar1L_Helpers = [
-                ql.FuturesRateHelper(ql.QuoteHandle(ql.SimpleQuote(_eurodollar.loc[ric][date])),
-                                     _eurodollar.loc[ric][MAT], lengthInMonths, calendar_country, ql.ModifiedFollowing,
-                                     True, dayCounter, convexityAdjustment) for ric in _eurodollar.index if 'EM' in ric]
-            lengthInMonths = 3
-            eurodollar3L_Helpers = [
-                ql.FuturesRateHelper(ql.QuoteHandle(ql.SimpleQuote(_eurodollar.loc[ric][date])),
-                                     _eurodollar.loc[ric][MAT], lengthInMonths, calendar_country, ql.ModifiedFollowing,
-                                     True, dayCounter, convexityAdjustment) for ric in _eurodollar.index if 'ED' in ric]
-        if product == SWAP_RATE:
-            _swap = LIBOR_product.loc[product].reset_index()
-            _swap1L = dict(zip(swap_1L_tenors,
-                               _swap.apply(lambda x: ql.SimpleQuote(x[date]) if '1L' in x['index'] else None,
-                                           axis=1).dropna()))
-            _swap3L = dict(zip(swap_3L_tenors,
-                               _swap.apply(lambda x: ql.SimpleQuote(x[date]) if '3L' in x['index'] else None,
-                                           axis=1).dropna()))
-            _swap6L = dict(zip(swap_1L_tenors,
-                               _swap.apply(lambda x: ql.SimpleQuote(x[date]) if '6L' in x['index'] else None,
-                                           axis=1).dropna()))
-            fixedLegFrequency = ql.Semiannual
-            fixedLegAdjustment = ql.Unadjusted
-            fixedLegDayCounter = ql.Actual365Fixed()
-            swap1L_Helpers = [
-                ql.SwapRateHelper(ql.QuoteHandle(_swap1L[tenor]), tenor, calendar_country, fixedLegFrequency,
-                                  fixedLegAdjustment, fixedLegDayCounter, ql.Euribor1M()) for tenor in swap_1L_tenors]
-            swap3L_Helpers = [
-                ql.SwapRateHelper(ql.QuoteHandle(_swap3L[tenor]), tenor, calendar_country, fixedLegFrequency,
-                                  fixedLegAdjustment, fixedLegDayCounter, ql.Euribor3M()) for tenor in swap_3L_tenors]
-            swap6L_Helpers = [
-                ql.SwapRateHelper(ql.QuoteHandle(_swap6L[tenor]), tenor, calendar_country, fixedLegFrequency,
-                                  fixedLegAdjustment, fixedLegDayCounter, ql.Euribor6M()) for tenor in swap_1L_tenors]
+
+    # deposits
+    _deposits = LIBOR_product.loc[DEPOSITS]
+    _deposits.index = deposit_maturities
+    _deposits[QUOTE] = _deposits.apply(lambda x: ql.SimpleQuote(x[date]), axis=1)
+    depositHelpers = [
+        ql.DepositRateHelper(ql.QuoteHandle(_deposits.loc[mat][QUOTE]), mat, settlementDays, calendar_country,
+                             ql.ModifiedFollowing, False, ql.Actual360()) for mat in deposit_maturities]
+    # fra
+    _fra = LIBOR_product.loc[FRA].reset_index()
+    _fra[TENOR] = _fra.apply(lambda x: int(x['index'].split('X')[1].replace('F=', '')) - int(
+        x['index'].split('X')[0].replace('USD', '')), axis=1)
+    _fra3L = dict(zip(FRA_3L_maturities,
+                      _fra[_fra[TENOR] == 3].apply(lambda x: ql.SimpleQuote(x[date]), axis=1).to_list()))
+    _fra6L = dict(zip(FRA_6L_maturities,
+                      _fra[_fra[TENOR] == 6].apply(lambda x: ql.SimpleQuote(x[date]), axis=1).to_list()))
+    fra3L_Helpers = [
+        ql.FraRateHelper(ql.QuoteHandle(_fra3L[mat]), mat[0], mat[1], settlementDays, calendar_country,
+                         ql.ModifiedFollowing, False, ql.Actual360()) for mat in FRA_3L_maturities if mat[0] < 12]
+    fra6L_Helpers = [
+        ql.FraRateHelper(ql.QuoteHandle(_fra6L[mat]), mat[0], mat[1], settlementDays, calendar_country,
+                         ql.ModifiedFollowing, False, ql.Actual360()) for mat in FRA_6L_maturities]
+    # eurodollar
+    _eurodollar = LIBOR_product.loc[EURODOLLAR_FUTURES]
+    _eurodollar[MAT] = _eurodollar.apply(lambda x: EURODOLLAR_maturities[x.name], axis=1)
+    _eurodollar[TOW] = _eurodollar.apply(
+        lambda x: (datetime(x[MAT].year(), x[MAT].month(), x[MAT].dayOfMonth()) - date).days, axis=1)
+    dayCounter = ql.Thirty360()
+    convexityAdjustment = ql.QuoteHandle(ql.SimpleQuote(0.01))
+    lengthInMonths = 1
+    eurodollar1L_Helpers = [
+        ql.FuturesRateHelper(ql.QuoteHandle(ql.SimpleQuote(_eurodollar.loc[ric][date])),
+                             _eurodollar.loc[ric][MAT], lengthInMonths, calendar_country, ql.ModifiedFollowing,
+                             True, dayCounter, convexityAdjustment) for ric in _eurodollar.index if
+        ('EM' in ric) & (_eurodollar.loc[ric][TOW] >= 25) & (_eurodollar.loc[ric][TOW] <= 365 * 2)]
+    lengthInMonths = 3
+    eurodollar3L_Helpers = [
+        ql.FuturesRateHelper(ql.QuoteHandle(ql.SimpleQuote(_eurodollar.loc[ric][date])),
+                             _eurodollar.loc[ric][MAT], lengthInMonths, calendar_country, ql.ModifiedFollowing,
+                             True, dayCounter, convexityAdjustment) for ric in _eurodollar.index if
+        ('ED' in ric) & (_eurodollar.loc[ric][TOW] >= 25) & (_eurodollar.loc[ric][TOW] <= 365)]
+    # swap
+    _swap = LIBOR_product.loc[SWAP_RATE].reset_index()
+    _swap1L = dict(zip(swap_1L_tenors,
+                       _swap.apply(lambda x: ql.SimpleQuote(x[date]) if '1L' in x['index'] else None,
+                                   axis=1).dropna()))
+    _swap3L = dict(zip(swap_3L_tenors,
+                       _swap.apply(lambda x: ql.SimpleQuote(x[date]) if '3L' in x['index'] else None,
+                                   axis=1).dropna()))
+    _swap6L = dict(zip(swap_1L_tenors,
+                       _swap.apply(lambda x: ql.SimpleQuote(x[date]) if '6L' in x['index'] else None,
+                                   axis=1).dropna()))
+    fixedLegFrequency = ql.Semiannual
+    fixedLegAdjustment = ql.Unadjusted
+    fixedLegDayCounter = ql.Actual365Fixed()
+    swap1L_Helpers = [
+        ql.SwapRateHelper(ql.QuoteHandle(_swap1L[tenor]), tenor, calendar_country, fixedLegFrequency,
+                          fixedLegAdjustment, fixedLegDayCounter, ql.Euribor1M()) for tenor in swap_1L_tenors]
+    swap3L_Helpers = [
+        ql.SwapRateHelper(ql.QuoteHandle(_swap3L[tenor]), tenor, calendar_country, fixedLegFrequency,
+                          fixedLegAdjustment, fixedLegDayCounter, ql.Euribor3M()) for tenor in swap_3L_tenors]
+    swap6L_Helpers = [
+        ql.SwapRateHelper(ql.QuoteHandle(_swap6L[tenor]), tenor, calendar_country, fixedLegFrequency,
+                          fixedLegAdjustment, fixedLegDayCounter, ql.Euribor6M()) for tenor in swap_1L_tenors]
 
     # term structure handles
     discountTermStructure = ql.RelinkableYieldTermStructureHandle()
     forecastTermStructure = ql.RelinkableYieldTermStructureHandle()
 
     # term-structure construction
-    helpers = depositHelpers[:2] + futuresHelpers + swapHelpers[1:]
-    depoFuturesSwapCurve = ql.PiecewiseFlatForward(settlementDate, helpers, Actual360())
+    helpers_1L = depositHelpers[:5] + eurodollar1L_Helpers + swap1L_Helpers
+    helpers_3L = depositHelpers[:5] + eurodollar3L_Helpers + swap3L_Helpers
+    helpers_6L = depositHelpers[:5] + fra6L_Helpers + swap6L_Helpers
 
-    helpers = depositHelpers[:3] + fraHelpers + swapHelpers
-    depoFraSwapCurve = ql.PiecewiseFlatForward(settlementDate, helpers, Actual360())
+    settlementDate = ql.Date(date.day, date.month, date.year)
+    day_count = ql.Actual360()
+    depoFuturesSwapCurve_1L = ql.PiecewiseCubicZero(0, calendar_country, helpers_1L, day_count)  # dc 확인하기
+    depoFuturesSwapCurve_1L.enableExtrapolation()
+    depoFuturesSwapCurve_3L = ql.PiecewiseCubicZero(0, calendar_country, helpers_3L, day_count)  # dc 확인하기
+    depoFuturesSwapCurve_3L.enableExtrapolation()
+    depoFuturesSwapCurve_6L = ql.PiecewiseCubicZero(0, calendar_country, helpers_6L, day_count)  # dc 확인하기
+    depoFuturesSwapCurve_6L.enableExtrapolation()
+
+    # The spot cpn is obtined from yieldcurve object using the zeroRate method.
+    for curve in [depoFuturesSwapCurve_1L, depoFuturesSwapCurve_3L, depoFuturesSwapCurve_6L]:
+        spots = []
+        tenors = []
+        for d in curve.dates():
+            yrs = day_count.yearFraction(settlementDate, d)
+            compounding = ql.Compounded
+            freq = ql.Annual
+            zero_rate = curve.zeroRate(yrs, compounding, freq)
+            tenors.append(yrs)
+            eq_rate = zero_rate.equivalentRate(day_count, compounding, freq, settlementDate, d).rate()
+            spots.append(100 * eq_rate)
+    ZC1L = pd.DataFrame(list(zip(tenors, spots)), columns=["Maturities", "Curve"], index=['']*len(tenors))
