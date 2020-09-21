3233ef46ba5ad44c4e947268079994e40d494b49
chan_star <traintion9@gmail.com>
2020-09-21 12:33:20 +0900

    IR multi-curve structure complete

--- interest_rate/multiple_curves.py
+++ interest_rate/multiple_curves.py
@@ -3,14 +3,10 @@
 :Author: Chankyu Choi
 :Date: 2020. 09. 17
 """
-from copy import deepcopy
-from datetime import datetime, timedelta
-
-import pandas as pd
-import QuantLib as ql
+from datetime import timedelta
 
 from trading_date.get_maturity import meetup_day
-from columns import *
+from interest_rate.quantlib_assistant_funcs import *
 
 # data preprocess
 LIBOR_TOTAL = pd.ExcelFile('data/interest_rate/LIBOR_TOTAL.xlsx')
@@ -79,7 +75,11 @@ settlementDays = 2
 
 # make curves every day
 for date in dates:
+    # setting time
     today = ql.Date(date.day, date.month, date.year)
+    ql.Settings.instance().evaluationDate = today
+
+    # select data
     LIBOR_product = LIBOR_products[LIBOR_products.index == date]
     LIBOR_product = LIBOR_product.dropna(axis='columns').T
 
@@ -103,14 +103,14 @@ for date in dates:
                          ql.ModifiedFollowing, False, ql.Actual360()) for mat in FRA_3L_maturities if mat[0] < 12]
     fra6L_Helpers = [
         ql.FraRateHelper(ql.QuoteHandle(_fra6L[mat]), mat[0], mat[1], settlementDays, calendar_country,
-                         ql.ModifiedFollowing, False, ql.Actual360()) for mat in FRA_6L_maturities]
+                         ql.ModifiedFollowing, False, ql.Actual360()) for mat in FRA_6L_maturities if mat[0] <= 12]
     # eurodollar
     _eurodollar = LIBOR_product.loc[EURODOLLAR_FUTURES]
     _eurodollar[MAT] = _eurodollar.apply(lambda x: EURODOLLAR_maturities[x.name], axis=1)
     _eurodollar[TOW] = _eurodollar.apply(
         lambda x: (datetime(x[MAT].year(), x[MAT].month(), x[MAT].dayOfMonth()) - date).days, axis=1)
     dayCounter = ql.Thirty360()
-    convexityAdjustment = ql.QuoteHandle(ql.SimpleQuote(0.01))
+    convexityAdjustment = ql.QuoteHandle(ql.SimpleQuote(0))
     lengthInMonths = 1
     eurodollar1L_Helpers = [
         ql.FuturesRateHelper(ql.QuoteHandle(ql.SimpleQuote(_eurodollar.loc[ric][date])),
@@ -122,7 +122,7 @@ for date in dates:
         ql.FuturesRateHelper(ql.QuoteHandle(ql.SimpleQuote(_eurodollar.loc[ric][date])),
                              _eurodollar.loc[ric][MAT], lengthInMonths, calendar_country, ql.ModifiedFollowing,
                              True, dayCounter, convexityAdjustment) for ric in _eurodollar.index if
-        ('ED' in ric) & (_eurodollar.loc[ric][TOW] >= 25) & (_eurodollar.loc[ric][TOW] <= 365)]
+        ('ED' in ric) & (_eurodollar.loc[ric][TOW] >= 25) & (_eurodollar.loc[ric][TOW] < 360 - 91)]
     # swap
     _swap = LIBOR_product.loc[SWAP_RATE].reset_index()
     _swap1L = dict(zip(swap_1L_tenors,
@@ -156,25 +156,111 @@ for date in dates:
     helpers_3L = depositHelpers[:5] + eurodollar3L_Helpers + swap3L_Helpers
     helpers_6L = depositHelpers[:5] + fra6L_Helpers + swap6L_Helpers
 
-    settlementDate = ql.Date(date.day, date.month, date.year)
     day_count = ql.Actual360()
     depoFuturesSwapCurve_1L = ql.PiecewiseCubicZero(0, calendar_country, helpers_1L, day_count)  # dc 확인하기
     depoFuturesSwapCurve_1L.enableExtrapolation()
     depoFuturesSwapCurve_3L = ql.PiecewiseCubicZero(0, calendar_country, helpers_3L, day_count)  # dc 확인하기
     depoFuturesSwapCurve_3L.enableExtrapolation()
-    depoFuturesSwapCurve_6L = ql.PiecewiseCubicZero(0, calendar_country, helpers_6L, day_count)  # dc 확인하기
-    depoFuturesSwapCurve_6L.enableExtrapolation()
+    depoFRASwapCurve_6L = ql.PiecewiseCubicZero(0, calendar_country, helpers_6L, day_count)  # dc 확인하기
+    depoFRASwapCurve_6L.enableExtrapolation()
 
-    # The spot cpn is obtined from yieldcurve object using the zeroRate method.
-    for curve in [depoFuturesSwapCurve_1L, depoFuturesSwapCurve_3L, depoFuturesSwapCurve_6L]:
-        spots = []
-        tenors = []
-        for d in curve.dates():
-            yrs = day_count.yearFraction(settlementDate, d)
-            compounding = ql.Compounded
-            freq = ql.Annual
-            zero_rate = curve.zeroRate(yrs, compounding, freq)
-            tenors.append(yrs)
-            eq_rate = zero_rate.equivalentRate(day_count, compounding, freq, settlementDate, d).rate()
-            spots.append(100 * eq_rate)
-    ZC1L = pd.DataFrame(list(zip(tenors, spots)), columns=["Maturities", "Curve"], index=['']*len(tenors))
+    # to DataFame
+    ZC1L = curve_to_DataFrame(depoFuturesSwapCurve_1L, today, curve_type='zero_curve', compound=ql.Continuous,
+                              daycounter=ql.Actual360(), under_name='1L')
+    DC1L = curve_to_DataFrame(depoFuturesSwapCurve_1L, today, curve_type='discount_curve', compound=ql.Continuous,
+                              under_name='1L')
+    FC1L = curve_to_DataFrame(depoFuturesSwapCurve_1L, today, curve_type='forward_curve', compound=ql.Continuous,
+                              daycounter=ql.Actual360(), forward_tenor=30, under_name='1L')
+    ZC3L = curve_to_DataFrame(depoFuturesSwapCurve_3L, today, curve_type='zero_curve', compound=ql.Continuous,
+                              daycounter=ql.Actual360(), under_name='3L')
+    DC3L = curve_to_DataFrame(depoFuturesSwapCurve_3L, today, curve_type='discount_curve', compound=ql.Continuous,
+                              under_name='3L')
+    FC3L = curve_to_DataFrame(depoFuturesSwapCurve_3L, today, curve_type='forward_curve', compound=ql.Continuous,
+                              daycounter=ql.Actual360(), forward_tenor=91, under_name='3L')
+    ZC6L = curve_to_DataFrame(depoFRASwapCurve_6L, today, curve_type='zero_curve', compound=ql.Continuous,
+                              daycounter=ql.Actual360(), under_name='6L')
+    DC6L = curve_to_DataFrame(depoFRASwapCurve_6L, today, curve_type='discount_curve', compound=ql.Continuous,
+                              under_name='6L')
+    FC6L = curve_to_DataFrame(depoFRASwapCurve_6L, today, curve_type='forward_curve', compound=ql.Continuous,
+                              daycounter=ql.Actual360(), forward_tenor=180, under_name='6L')
+    ZC = pd.concat([ZC1L, ZC3L['3L_zero_rate'], ZC6L['6L_zero_rate']], axis=1).set_index(DATE)
+    DC = pd.concat([DC1L, DC3L['3L_discount'], DC6L['6L_discount']], axis=1).set_index(DATE)
+    FC = pd.concat([FC1L, FC3L['3L_forward_rate'], FC6L['6L_forward_rate']], axis=1).set_index(DATE)
+
+    # plot curves
+    plot_curve(ZC, start_date=today, end_date=today + ql.Period(3, ql.Years), ylim=[-0.01, 0.02])
+    plot_curve(DC, start_date=today, end_date=today + ql.Period(3, ql.Years), ylim=[-0.01, 0.02])
+    plot_curve(FC, start_date=today, end_date=today + ql.Period(3, ql.Years), ylim=[-0.015, 0.03])
+
+    plot_curve(ZC, start_date=today, end_date=ZC.index[-1], ylim=[ZC.values.min() - 0.005, ZC.values.max() + 0.005])
+    plot_curve(DC, start_date=today, end_date=ZC.index[-1], ylim=[-0.01, 0.07])
+    plot_curve(FC, start_date=today, end_date=ZC.index[-1], ylim=[FC.values.min() - 0.005, FC.values.max() + 0.005])
+
+# def get_helpers(IR_product, date, deposits=False, fra=False, futures=False, swap=False):
+#     IR_product = IR_product[IR_product.index == date]
+#     IR_product = IR_product.dropna(axis='columns').T
+#
+#     if deposits:
+#         _deposits = IR_product.loc[DEPOSITS]
+#         _deposits.index = deposit_maturities
+#         _deposits[QUOTE] = _deposits.apply(lambda x: ql.SimpleQuote(x[date]), axis=1)
+#         depositHelpers = [
+#             ql.DepositRateHelper(ql.QuoteHandle(_deposits.loc[mat][QUOTE]), mat, settlementDays, calendar_country,
+#                                  ql.ModifiedFollowing, False, ql.Actual360()) for mat in deposit_maturities]
+#     if fra:
+#         _fra = IR_product.loc[FRA].reset_index()
+#         _fra[TENOR] = _fra.apply(lambda x: int(x['index'].split('X')[1].replace('F=', '')) - int(
+#             x['index'].split('X')[0].replace('USD', '')), axis=1)
+#         _fra3L = dict(zip(FRA_3L_maturities,
+#                           _fra[_fra[TENOR] == 3].apply(lambda x: ql.SimpleQuote(x[date]), axis=1).to_list()))
+#         _fra6L = dict(zip(FRA_6L_maturities,
+#                           _fra[_fra[TENOR] == 6].apply(lambda x: ql.SimpleQuote(x[date]), axis=1).to_list()))
+#         fra3L_Helpers = [
+#             ql.FraRateHelper(ql.QuoteHandle(_fra3L[mat]), mat[0], mat[1], settlementDays, calendar_country,
+#                              ql.ModifiedFollowing, False, ql.Actual360()) for mat in FRA_3L_maturities if mat[0] < 12]
+#         fra6L_Helpers = [
+#             ql.FraRateHelper(ql.QuoteHandle(_fra6L[mat]), mat[0], mat[1], settlementDays, calendar_country,
+#                              ql.ModifiedFollowing, False, ql.Actual360()) for mat in FRA_6L_maturities]
+#     if futures:
+#         _eurodollar = IR_product.loc[EURODOLLAR_FUTURES]
+#         _eurodollar[MAT] = _eurodollar.apply(lambda x: EURODOLLAR_maturities[x.name], axis=1)
+#         _eurodollar[TOW] = _eurodollar.apply(
+#             lambda x: (datetime(x[MAT].year(), x[MAT].month(), x[MAT].dayOfMonth()) - date).days, axis=1)
+#         dayCounter = ql.Thirty360()
+#         convexityAdjustment = ql.QuoteHandle(ql.SimpleQuote(0))
+#         lengthInMonths = 1
+#         eurodollar1L_Helpers = [
+#             ql.FuturesRateHelper(ql.QuoteHandle(ql.SimpleQuote(_eurodollar.loc[ric][date])),
+#                                  _eurodollar.loc[ric][MAT], lengthInMonths, calendar_country, ql.ModifiedFollowing,
+#                                  True, dayCounter, convexityAdjustment) for ric in _eurodollar.index if
+#             ('EM' in ric) & (_eurodollar.loc[ric][TOW] >= 25) & (_eurodollar.loc[ric][TOW] <= 365 * 2)]
+#         lengthInMonths = 3
+#         eurodollar3L_Helpers = [
+#             ql.FuturesRateHelper(ql.QuoteHandle(ql.SimpleQuote(_eurodollar.loc[ric][date])),
+#                                  _eurodollar.loc[ric][MAT], lengthInMonths, calendar_country, ql.ModifiedFollowing,
+#                                  True, dayCounter, convexityAdjustment) for ric in _eurodollar.index if
+#             ('ED' in ric) & (_eurodollar.loc[ric][TOW] >= 25) & (_eurodollar.loc[ric][TOW] <= 365)]
+#     if swap:
+#         _swap = IR_product.loc[SWAP_RATE].reset_index()
+#         _swap1L = dict(zip(swap_1L_tenors,
+#                            _swap.apply(lambda x: ql.SimpleQuote(x[date]) if '1L' in x['index'] else None,
+#                                        axis=1).dropna()))
+#         _swap3L = dict(zip(swap_3L_tenors,
+#                            _swap.apply(lambda x: ql.SimpleQuote(x[date]) if '3L' in x['index'] else None,
+#                                        axis=1).dropna()))
+#         _swap6L = dict(zip(swap_1L_tenors,
+#                            _swap.apply(lambda x: ql.SimpleQuote(x[date]) if '6L' in x['index'] else None,
+#                                        axis=1).dropna()))
+#         fixedLegFrequency = ql.Semiannual
+#         fixedLegAdjustment = ql.Unadjusted
+#         fixedLegDayCounter = ql.Actual365Fixed()
+#         swap1L_Helpers = [
+#             ql.SwapRateHelper(ql.QuoteHandle(_swap1L[tenor]), tenor, calendar_country, fixedLegFrequency,
+#                               fixedLegAdjustment, fixedLegDayCounter, ql.Euribor1M()) for tenor in swap_1L_tenors]
+#         swap3L_Helpers = [
+#             ql.SwapRateHelper(ql.QuoteHandle(_swap3L[tenor]), tenor, calendar_country, fixedLegFrequency,
+#                               fixedLegAdjustment, fixedLegDayCounter, ql.Euribor3M()) for tenor in swap_3L_tenors]
+#         swap6L_Helpers = [
+#             ql.SwapRateHelper(ql.QuoteHandle(_swap6L[tenor]), tenor, calendar_country, fixedLegFrequency,
+#                               fixedLegAdjustment, fixedLegDayCounter, ql.Euribor6M()) for tenor in swap_1L_tenors]
+#     return
---
+++ interest_rate/quantlib_assistant_funcs.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Chankyu Choi
+:Date: 2020. 09. 17
+"""
+
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.dates import MonthLocator, DateFormatter
+import QuantLib as ql
+
+from columns import *
+
+
+def to_datetime(d):
+    return datetime(d.year(), d.month(), d.dayOfMonth())
+
+
+def format_rate(r):
+    return '%.4f %%' % (r.rate() * 100.0)
+
+
+def curve_to_DataFrame(curves, today, curve_type, compound, daycounter=None, forward_tenor=0, under_name=None):
+    _dates = [today + ql.Period(i, ql.Days) for i in range(0, curves.dates()[-1] - today)]
+    valid_dates = [d for d in _dates if d >= curves.referenceDate()]
+    if curve_type == 'zero_curve':
+        if not daycounter:
+            ValueError("setting daycount convention")
+        rates = [curves.zeroRate(d, daycounter, compound).rate() for d in valid_dates]
+        return pd.DataFrame(np.array([valid_dates, rates]).T, columns=[DATE, under_name + '_zero_rate'])
+    if curve_type == 'discount_curve':
+        rates = [curves.discount(d) for d in valid_dates]
+        return pd.DataFrame(np.array([valid_dates, rates]).T, columns=[DATE, under_name + '_discount'])
+    if curve_type == 'forward_curve':
+        if forward_tenor == 0:
+            ValueError("forward_tenor must be greater than 0")
+        rates = [curves.forwardRate(d, d + forward_tenor, daycounter, compound).rate() for d in valid_dates]
+        return pd.DataFrame(np.array([valid_dates, rates]).T, columns=[DATE, under_name + '_forward_rate'])
+    else:
+        ValueError("check curve type")
+
+
+def plot_curve(curves, start_date: ql.Date, end_date: ql.Date, ylim: list = None):
+    if ylim is None:
+        ylim = [-0.01, 0.07]
+    _dates = [d for d in curves.index if (start_date <= d) & (d <= end_date)]
+    fig, ax = plt.subplots()
+    plt.rc('lines', linewidth=3)
+    for idx, value in curves.iteritems():
+        ax.plot_date([to_datetime(d) for d in _dates], value.loc[_dates].values, '-')
+    ax.set_xlim(to_datetime(min(_dates)), to_datetime(max(_dates)))
+    ax.xaxis.set_major_locator(MonthLocator(bymonth=[12]))
+    ax.xaxis.set_major_formatter(DateFormatter("%b '%y"))
+    ax.set_ylim(ylim[0], ylim[1])
+    ax.autoscale_view()
+    ax.xaxis.grid(True, 'major')
+    ax.xaxis.grid(False, 'minor')
+    ax.legend(curves.columns)
+    ax.set_title(start_date)
+    fig.autofmt_xdate()
+    plt.show()
